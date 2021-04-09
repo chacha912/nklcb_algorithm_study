c63719506742aca13eca80be4221dc747b095da6
dooyeong20 <noviced22@gmail.com>
2021-04-09 20:57:34 +0900

    feat: Init BOJ_2573

---
+++ week02/BOJ_2573_빙산/BOJ_2573_빙산_정두영.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+
+def solution(N, M, board):
+    ans = 0
+
+    return ans
+
+
+N, M = map(int, stdin.readline().split())
+board = [list(map(int, stdin.readline().split())) for _ in range(N)]
+
+print(solution(N, M, board))
+
+'''
+5 7
+0 0 0 0 0 0 0
+0 2 4 5 3 0 0
+0 3 0 2 5 2 0
+0 7 6 2 4 0 0
+0 0 0 0 0 0 0
+
+2
+
+'''
