2b56e7c1fde9ad715df3fd133386068730f9f751
dooyeong20 <noviced22@gmail.com>
2021-04-12 10:03:44 +0900

    fix: Fixed code and Solved
    reduced time complexity

--- week02/BOJ_2573_빙산/BOJ_2573_빙산_정두영.py
+++ week02/BOJ_2573_빙산/BOJ_2573_빙산_정두영.py
@@ -3,29 +3,6 @@ from collections import deque
 
 
 def solution(N, M, board):
-    def check():
-        def bfs(visited, i, j):
-            q = deque([(i, j)])
-            visited.add((i, j))
-
-            while q:
-                r, c = q.popleft()
-                for d in dirs:
-                    nr, nc = r + d[0], c + d[1]
-
-                    if 0 <= nr < N and 0 <= nc < M and board[nr][nc] > 0 and (nr, nc) not in visited:
-                        visited.add((nr, nc))
-                        q.append((nr, nc))
-
-        visited = set()
-        cnt = 0
-        for i in range(N):
-            for j in range(M):
-                if board[i][j] > 0 and (i, j) not in visited:
-                    bfs(visited, i, j)
-                    cnt += 1
-        return cnt
-
     def melt():
         def bfs(visited, i, j):
             q = deque([(i, j)])
@@ -47,11 +24,13 @@ def solution(N, M, board):
                 board[r][c] = [board[r][c], cnt]
 
         visited = set()
+        cnt = 0
 
         for i in range(N):
             for j in range(M):
                 if board[i][j] != 0 and (i, j) not in visited:
                     bfs(visited, i, j)
+                    cnt += 1
 
         for i in range(N):
             for j in range(M):
@@ -59,19 +38,18 @@ def solution(N, M, board):
                     gap = board[i][j][0] - board[i][j][1]
                     board[i][j] = gap if gap > 0 else 0
 
+        return cnt
+
     ans = 0
 
     while 1:
-        melt()
-        ans += 1
-
-        c = check()
-
-        if c == 0:
+        cnt = melt()
+        if cnt == 0:
             return 0
 
-        if c > 1:
+        if cnt > 1:
             break
+        ans += 1
 
     return ans
 
@@ -80,30 +58,3 @@ N, M = map(int, stdin.readline().split())
 board = [list(map(int, stdin.readline().split())) for _ in range(N)]
 dirs = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 print(solution(N, M, board))
-
-'''
-5 7
-0 0 0 0 0 0 0
-0 2 4 5 3 0 0
-0 3 0 2 5 2 0
-0 7 6 2 4 0 0
-0 0 0 0 0 0 0
-
-5 7
-0 0 0 0 0 0 0
-0 2 0 0 0 0 0
-0 0 0 0 0 0 0
-0 0 0 0 0 0 0
-0 0 0 0 0 0 0
-
-5 7
-0 0 0 0 0 0 0
-0 3 3 1 3 3 0
-0 4 0 4 0 3 0
-0 0 0 0 4 3 0
-0 0 0 0 0 0 0
-1
-
-2
-
-'''
