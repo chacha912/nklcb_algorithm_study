ac3436932bbc2e46c5a5d03bc39f2c242da5a027
chacha912 <dkwkdnwk912@gmail.com>
2021-05-21 00:17:52 +0900

    feat: Add BOJ_10942 solution using DP by chacha912

--- week06/BOJ_10942_팰린드롬/BOJ_10942_yourim.py
+++ week06/BOJ_10942_팰린드롬/BOJ_10942_yourim.py
@@ -4,20 +4,34 @@ N = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 
-def chkPalindrome(arr):
-    s = 0 
-    e = len(arr) - 1
-    while s < e:
-        if arr[s] != arr[e]:
-            return False
-        s += 1
-        e -= 1
-    return True    
+dp = [dict() for _ in range(N+1)]
+# dp = [[-1]*(N+1) for _ in range(N+1)]  
+
+def chkPalindrome(start, end):
+    
+    if end in dp[start]: 
+        return dp[start][end]
+
+    # if dp[start][end] != -1:
+    #     return dp[start][end]
+
+    if start == end:
+        dp[start][end] = 1
+        return 1
+
+    if arr[start] != arr[end]:
+        dp[start][end] = 0
+        return 0
+
+    if start == end - 1: 
+        dp[start][end] = 1
+        return 1
+    
+    dp[start][end] = chkPalindrome(start+1, end-1)
+
+    return dp[start][end] 
+
 
 for _ in range(M):
     start, end = map(int, sys.stdin.readline().split())
-    chk = chkPalindrome(arr[start-1:end])
-    if chk is True:
-        print(1)
-    else:
-        print(0)
+    print(chkPalindrome(start-1, end-1))
